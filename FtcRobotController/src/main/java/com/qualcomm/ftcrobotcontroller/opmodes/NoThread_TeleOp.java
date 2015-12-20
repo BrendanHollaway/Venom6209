60b397b03d681fcf54da763b7e43b8017ddd8c65
BrendanHollaway <blacklhorns@gmail.com>
2015-12-20 15:03:45 -0600

    stuf
    k coo

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/NoThread_TeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/NoThread_TeleOp.java
@@ -46,7 +46,6 @@ public class NoThread_TeleOp extends OpMode {
     AdafruitIMU gyroAcc;
     volatile double[] rollAngle = new double[2], pitchAngle = new double[2], yawAngle = new double[2];
     double[] accel = new double[3];
-    //
 
     //Gamepad localGamepad1;
     //Gamepad localGamepad2;
@@ -181,25 +180,15 @@ public class NoThread_TeleOp extends OpMode {
                 yToggle = 3;
             }
         }
-        //If A button is pressed
-        //If enableSOS is false, then SOS is not enabled, and thus the robot will not move backwards by itself
-        // even if the robot is tilting over.
-        //If it's true, then it'll save your bacon
-        //if greater then 55, then SOS may need to be disabled
         if (aButton1) {
-            //if SOS is not enabled
             if (!enableSOS) {
-                //increase the toggle count
                 SOS_toggle_count++;
             }
-            //turns SOS off; SOS will not run
             else {
                 enableSOS = false;
             }
-            //if the count is greater than 1
             if(SOS_toggle_count > 1)
             {
-                //reset toggle count and turn on SOS
                 SOS_toggle_count = 0;
                 enableSOS = true;
             }
