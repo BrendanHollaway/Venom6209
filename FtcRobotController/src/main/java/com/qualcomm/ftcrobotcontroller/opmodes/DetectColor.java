b425e0886a7abcdc8a6340034d7534a8ab5e7e9a
BrendanHollaway <blacklhorns@gmail.com>
2016-01-02 14:34:59 -0600

    Take a Picture Enabled
    Added a function to DetectColor.java, such that when button A is
    pressed, it will take the image the phone has, and record it as a 80x60
    array of colors, specifically, R, G, or B.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DetectColor.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DetectColor.java
@@ -40,34 +40,47 @@ public class DetectColor extends OpModeCamera {
         long startTime = System.currentTimeMillis();
 
         if (imageReady()) { // only do this if an image has been returned from the camera
-            int redValue = 0;
-            int blueValue = 0;
-            int greenValue = 0;
-
-            Bitmap rgbImage;
-            rgbImage = convertYuvImageToRgb(yuvImage, width, height, ds2);
-            for (int x = 0; x < width / ds2; x++) {
-                for (int y = 0; y < height / ds2; y++) {
-                    int pixel = rgbImage.getPixel(x, y);
-                    redValue += red(pixel);
-                    blueValue += blue(pixel);
-                    greenValue += green(pixel);
+            if(gamepad1.a)
+            {
+                Bitmap rgbImage;
+                rgbImage = convertYuvImageToRgb(yuvImage, width, height, ds2);
+                String output = "";
+                for (int x = 0; x < width / ds2; x++) {
+                    for (int y = 0; y < height / ds2; y++) {
+                        int pixel = rgbImage.getPixel(x, y);
+                        output += color(pixel);
+                    }
                 }
-            }
-            int color = highestColor(redValue, greenValue, blueValue);
-            String colorString = "";
-            switch (color) {
-                case 0:
-                    colorString = "RED";
-                    break;
-                case 1:
-                    colorString = "GREEN";
-                    break;
-                case 2:
-                    colorString = "BLUE";
-            }
-            telemetry.addData("Color:", "Color detected is: " + colorString);
+                DbgLog.msg("My Name is Bo, Run #" + ++looped +": " + output);
+                int redValue = 0;
+                int blueValue = 0;
+                int greenValue = 0;
 
+                Bitmap rgbImage;
+                rgbImage = convertYuvImageToRgb(yuvImage, width, height, ds2);
+                for (int x = 0; x < width / ds2; x++) {
+                    for (int y = 0; y < height / ds2; y++) {
+                        int pixel = rgbImage.getPixel(x, y);
+                        redValue += red(pixel);
+                        blueValue += blue(pixel);
+                        greenValue += green(pixel);
+                    }
+                }
+                int color = highestColor(redValue, greenValue, blueValue);
+                String colorString = "";
+                switch (color) {
+                    case 0:
+                        colorString = "RED";
+                        break;
+                    case 1:
+                        colorString = "GREEN";
+                        break;
+                    case 2:
+                        colorString = "BLUE";
+                }
+                Telemetry.addData("Color: ", colorString);
+                telemetry.addData("My Name is Bo, Run #", looped +": " + output);
+            }
         }
         long endTime = System.currentTimeMillis();
         telemetry.addData("Dims", Integer.toString(width / ds2) + " x " + Integer.toString(height / ds2));
@@ -80,5 +93,17 @@ public class DetectColor extends OpModeCamera {
     @Override
     public void stop() {
         super.stop(); // stops camera functions
+    }
+    public String color(int pixel)
+    {
+        byte red = (byte)(pixel >> 16 & 0xFF);
+        byte green = (byte)(pixel >> 8 & 0xFF);
+        byte blue = (byte)(pixel & 0xFF);
+        byte max = (byte) Math.max(red, Math.max(green,blue));
+        if(red == max)
+            return "R";
+        if(blue == max)
+            return "B";
+        return "G";
     }
 }
