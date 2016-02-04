4cd1b601742654a22f1349e25362e77d3b54bb5a
BrendanHollaway <blacklhorns@gmail.com>
2016-02-04 16:30:46 -0600

    TestTeleOp & Pre update commit
    added more movement to TestTeleOp
    about to update sdk to ver 16.01.04.001

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestTeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestTeleOp.java
@@ -10,20 +10,67 @@ public class TestTeleOp extends OpMode{
 
     DcMotor motorFR;
     DcMotor motorFL;
-    DcMotor motorBR;
-    DcMotor motorBL;
-    DcMotor motorPPR;
-    DcMotor motorPPL;
+    //DcMotor motorBR;
+    //DcMotor motorBL;
+    DcMotor liftL;
+    DcMotor liftR;
     public void init(){
-        motorBL = hardwareMap.dcMotor.get("bl");
+        //motorBL = hardwareMap.dcMotor.get("bl");
         motorFL = hardwareMap.dcMotor.get("fl");
-        motorBR = hardwareMap.dcMotor.get("br");
+        //motorBR = hardwareMap.dcMotor.get("br");
         motorFR = hardwareMap.dcMotor.get("fr");
-        motorPPR = hardwareMap.dcMotor.get("ppr");
-        motorPPL = hardwareMap.dcMotor.get("ppl");
+        liftL = hardwareMap.dcMotor.get("liftL");
+        liftR = hardwareMap.dcMotor.get("liftR");
     }
     public void loop(){
-        if(gamepad1.a)
+        if(Math.abs(gamepad2.left_stick_y) > 0.1 && Math.abs(gamepad2.right_stick_y) > 0.1)
+        {
+            liftL.setPower(gamepad2.right_stick_y);
+            liftR.setPower(gamepad2.left_stick_y);
+        }
+        else if(Math.abs(gamepad2.right_stick_y) > 0.1)
+        {
+            liftL.setPower(gamepad2.right_stick_y);
+            liftR.setPower(0);
+        }
+        else if(Math.abs(gamepad2.left_stick_y) > 0.1)
+        {
+            liftL.setPower(0);
+            liftR.setPower(gamepad2.left_stick_y);
+        }
+        else
+        {
+            liftL.setPower(0);
+            liftR.setPower(0);
+        }
+        if (Math.abs(gamepad1.left_stick_y) > 0.1 && Math.abs(gamepad1.right_stick_y) > 0.1) {
+            motorFR.setPower(gamepad1.left_stick_y);
+            motorFL.setPower(-gamepad1.right_stick_y);
+            //motorBR.setPower(gamepad1.left_stick_y);
+            //motorBL.setPower(-gamepad1.right_stick_y);
+        } else if (Math.abs(gamepad1.left_stick_y) > 0.1) {
+            motorFR.setPower(gamepad1.left_stick_y);
+            motorFL.setPower(0);
+            //motorBR.setPower(gamepad1.left_stick_y);
+            //motorBL.setPower(0);
+        } else if (Math.abs(gamepad1.right_stick_y) > 0.1) {
+            motorFR.setPower(0);
+            motorFL.setPower(-gamepad1.right_stick_y);
+            //motorBR.setPower(0);
+            //motorBL.setPower(-gamepad1.right_stick_y);
+        } else if(gamepad1.left_trigger > 0.1 || gamepad1.right_trigger > 0.1)
+        {
+            //motorBR.setPower(gamepad1.right_trigger > 0.1? gamepad1.right_trigger : 0);
+            //motorBL.setPower(gamepad1.left_trigger > 0.1? -gamepad1.left_trigger : 0);
+        }
+        else {
+            motorFR.setPower(0);
+            motorFL.setPower(0);
+            //motorBR.setPower(0);
+            //motorBL.setPower(0);
+        }
+            
+        /*if(gamepad1.a)
             motorFR.setPower(1.0); //need to reverse
         else
             motorFR.setPower(0.0);
@@ -31,7 +78,7 @@ public class TestTeleOp extends OpMode{
             motorFL.setPower(1.0);
         else
             motorFL.setPower(0.0);
-        if(gamepad1.x)
+        /*if(gamepad1.x)
             motorBR.setPower(1.0);
         else
             motorBR.setPower(0.0);
@@ -46,6 +93,6 @@ public class TestTeleOp extends OpMode{
         if(gamepad1.right_bumper)
             motorPPR.setPower(1.0);
         else
-            motorPPR.setPower(0.0);
+            motorPPR.setPower(0.0);*/
     }
 }
